7f6e3c3b50f4c011691dd428de8b27ebc55eff36
EgorFabrichkin <70843062+EgorFabrichkin@users.noreply.github.com>
2020-10-17 15:48:29 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrElem = [], arrWord = [];
+
+    for (let i = 0; i < expr.length; i+=10) {
+        arrElem.push(expr.slice(i, i + 10));
+    }
+    
+    for (let i = 0; i < arrElem.length; i++) {
+        if (arrElem[i] === '**********') {
+            arrWord.push(' ');
+        } else {
+            arrWord.push(decodeElement(arrElem[i]));
+        }
+    }
+    return arrWord.join('');
+}
+
+function decodeElement(arr) {
+    let wordCode = arr.substring(arr.indexOf('1'));
+    let wordMorze = [];
+    for (let i = 0; i < wordCode.length; i+=2) {
+        if (wordCode.substring(i, i + 2) === '10') {
+            wordMorze.push('.');
+        } else { 
+            wordMorze.push('-');
+        }
+    }
+    return MORSE_TABLE[wordMorze.join('')]
 }
 
 module.exports = {
-    decode
+   decode
 }
